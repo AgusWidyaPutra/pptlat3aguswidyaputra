ce16f8ae10d47695a96f41932489ee47d88d02cd
AgusWidyaPutra <awp170802@gmail.com>
2021-09-29 22:23:01 +0700

    pptLat3AgusWidyaPutra
    PPT LATIHAN 3 / SLIDE 20 . AGUS WIDYA PUTRA

---
+++ lat3.php
@@ -0,0 +1,18 @@
+<HTML>
+<HEAD>
+<TITLE> Contoh Skirp PHP </TITLE>
+<BODY>
+<? 
+        $a = 3;
+        $b = 7;
+        echo '$a * $b = ';
+        print $a*$b;
+        echo "<BR>";
+        echo '$a / $b = ';
+        print $a/$b;
+        echo "<BR>";
+        echo '$a % $b = ';
+        print $a%$b;
+?>
+</BODY>
+</HTML>
